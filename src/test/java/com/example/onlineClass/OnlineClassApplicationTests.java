3017db08bd7d301b7935fdc14689e68ee08d8385
seunghoon_jeong <seunghoon_jeong@tmax.co.kr>
2022-04-22 12:59:29 +0900

    Initial commit

---
+++ src/test/java/com/example/onlineClass/OnlineClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
